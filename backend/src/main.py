106639b8f0ae97b45e8f3d877b6d414cb2e98603
Erick <erickdambros449@gmail.com>
2026-08-10 18:54:10 -0400

    Estrutura primária do backend

---
+++ backend/src/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+
+# Inicializa o aplicativo FastAPI com algumas informações básicas
+app = FastAPI(
+    title="Meu Projeto API",
+    description="Backend estruturado com FastAPI, SQLAlchemy e MySQL",
+    version="0.1.0",
+)
+
+# Cria uma rota básica de teste para saber se está tudo funcionando
+@app.get("/")
+async def root():
+    return {
+        "status": "sucesso",
+        "mensagem": "A API está rodando perfeitamente!"
+    }
+
