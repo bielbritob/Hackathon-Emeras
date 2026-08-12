a0776e0779898c39c6d837f88139ee789aa490e0
bielbritob <bielbritob123@gmail.com>
2026-08-11 23:32:19 -0400

    add: minimal setup in v1/api

--- backend/app/api/v1/api.py
+++ backend/app/api/v1/api.py
@@ -1,2 +1,17 @@
-# aq vai o v1
-#teeesteeeee
+from fastapi import FastAPI
+
+# Inicializa o aplicativo FastAPI com algumas informações básicas
+app = FastAPI(
+    title="Meu Projeto API",
+    description="Backend estruturado com FastAPI, SQLAlchemy e MySQL",
+    version="0.1.0",
+)
+
+# Cria uma rota básica de teste para saber se está tudo funcionando
+@app.get("/documentos")
+async def root():
+    return {
+        "status": "sucesso",
+        "mensagem": "A API está rodando perfeitamente!"
+    }
+# booommmm
